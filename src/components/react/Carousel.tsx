7c232d4372b1228b1f97193b537b2e59bddf0218
Sir-Thom <Sir-Thom1702@proton.me>
2023-06-15 17:28:49 -0400

    base du nouveau site fini

--- src/components/react/Carousel.tsx
+++ src/components/react/Carousel.tsx
@@ -54,20 +54,23 @@ function Carousel(CarouselProps: Content) {
       <div className="flex lg:w-[42rem] sm:w-32 justify-center">
         <button
           aria-label="Previous card"
-          className="absolute mr-4 left-0 top-1/2 transform -translate-y-1/2 rounded-full hover:bg-neutral-300 focus:bg-neutral-300 transition-colors duration-300"
+          className="absolute mr-4 left-0  top-1/2 transform -translate-y-1/2 rounded-full  hover:scale-110 active:scale-90 transition-all duration-300"
           onClick={handlePrev}
         >
           <MdOutlineNavigateBefore
             size={48}
-            className="stroke-current mr-4 left--4"
+            className="stroke-current  mr-4 left--4"
           />
         </button>
         <button
           aria-label="Next card"
-          className="absolute mr-4 right-0 top-1/2 transform -translate-y-1/2 rounded-full hover:bg-neutral-300 focus:bg-neutral-300 transition-colors duration-300"
+          className="absolute mr-4 right-0 top-1/2 transform -translate-y-1/2 rounded-full   hover:scale-110 active:scale-90 transition-all duration-300"
           onClick={handleNext}
         >
-          <MdOutlineNavigateNext size={48} className="stroke-current ml-4" />
+          <MdOutlineNavigateNext
+            size={48}
+            className="transition-colors hover:stroke-DigitalOceanBlue-50 active:stroke-DigitalOceanBlue-100 stroke-white ml-4"
+          />
         </button>
       </div>
       <div className="flex items-center justify-center h-auto w-auto">
