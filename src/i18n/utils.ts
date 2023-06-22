abcb1e78ea98cfd763a6674177c989985cfdc509
Sir-Thom <Sir-Thom1702@proton.me>
2023-06-22 16:09:02 -0400

    translation finished

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -3,18 +3,14 @@ import { ui, defaultLang } from "./ui.ts";
 export function getLangFromUrl(url: URL) {
 	const [, lang] = url.pathname.split("/");
 	if (lang in ui) return lang as keyof (typeof ui)[];
-	console.log(`Language ${lang} `);
 	console.warn(`Language ${lang} not found, using default language ${defaultLang}`);
 	return defaultLang;
 }
 export function getURLFormReact(): string {
 	if (typeof window !== "undefined") {
 		const url = new URL(window.location.href);
-		console.log(url);
 		const [, lang] = url.pathname.split("/");
-		console.log(lang);
 		if (lang in ui) return lang as keyof typeof ui;
-		console.log(`Language ${lang}`);
 	}
 
 	console.warn(`Language not found, using default language ${defaultLang}`);
