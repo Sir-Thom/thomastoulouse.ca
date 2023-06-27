49e31b8a998b24a52eb2d5e7d3f28610751a947d
Sir-Thom <Sir-Thom1702@proton.me>
2023-06-27 10:13:52 -0400

    bugfixes url for license when switching langages with tag

--- src/components/react/LanguagesSelector.tsx
+++ src/components/react/LanguagesSelector.tsx
@@ -1,29 +1,29 @@
 import { AiOutlineGlobal } from "react-icons/ai";
 import { languages } from "../../i18n/ui";
-import { getLangFromUrl, getURLFormReact } from "../../i18n/utils";
-
-const langages = getURLFormReact();
-const url = new URL(window.location.href);
-const currentLanguage: any = getLangFromUrl(url); // Get the current language from the URL
+import { getLangFromUrl } from "../../i18n/utils";
 
 export default function LanguageSelector() {
+	const url = new URL(window.location.href);
+	const currentLanguage = getLangFromUrl(url); // Get the current language from the URL
+
 	const handleLanguageChange = (event) => {
 		const selectedLanguageCode = event.target.value;
+		const currentPathname = url.pathname;
+		const currentHash = url.hash;
+		// some big ball regex skill flexing there :D
+		const newPathname = currentPathname.replace(/\/\w+(\/|$)/, `/${selectedLanguageCode}$1`);
 
-		const url = new URL(window.location.href);
-		console.log(url);
-		let parts = window.location.href.split("/");
-		const currentpage = parts[parts.length - 1];
-		console.log(currentpage);
-		url.href = url.href.replace(url.pathname, `/${selectedLanguageCode}/${currentpage}`);
-		window.location.href = url.toString();
+		const newUrl = `${url.origin}${newPathname}${currentHash}`;
+
+		// Redirect the user to the new URL
+		window.location.href = newUrl;
 	};
 
 	return (
-		<div className="flex items-center rounded-xl bg-transparent   dark:bg-transparent ">
+		<div className="flex items-center rounded-xl bg-transparent dark:bg-transparent">
 			<AiOutlineGlobal size={24} />
 			<select
-				className="ml-2 rounded-xl bg-transparent  py-2  dark:bg-transparent  "
+				className="ml-2 rounded-xl bg-transparent py-2 dark:bg-transparent"
 				aria-label="languages"
 				name="languages"
 				id="lang"
@@ -32,7 +32,7 @@ export default function LanguageSelector() {
 			>
 				{Object.entries(languages)
 					.map(([lang, label]) => (
-						<option aria-label={langages} key={lang} defaultValue={lang} value={lang}>
+						<option aria-label={label} key={lang} value={lang}>
 							{lang}
 						</option>
 					))
