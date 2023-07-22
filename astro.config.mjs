e46560065410802ad15429b92f7d984202bdd8f1
Sir-Thom <Sir-Thom1702@proton.me>
2023-07-21 22:51:37 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -36,9 +36,9 @@ const { VITE_FIREBASE_MEASUREMENT_ID } = loadEnv(
 // https://astro.build/config
 export default defineConfig({
 	
-	site: 'https://www.thomastoulouse.ca',
+	
 	integrations: [tailwind(), react()],
-	output: "server",
+	output: "hybrid",
 	adapter: netlify(),
 	env: {
 		// Define your environment variables here
