48cffa39d4ee5c1e6d026e299341ee6b0dd33ab5
Sir-Thom <Sir-Thom1702@proton.me>
2023-07-21 22:53:42 -0400

    fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -36,7 +36,7 @@ const { VITE_FIREBASE_MEASUREMENT_ID } = loadEnv(
 // https://astro.build/config
 export default defineConfig({
 	
-	
+	site: 'https://www.thomastoulouse.ca',
 	integrations: [tailwind(), react()],
 	output: "hybrid",
 	adapter: netlify(),
