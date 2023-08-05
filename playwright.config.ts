1218d3bdf751264c2a10a5bb6d8ccce72235a0e2
Sir-Thom <90110920+Sir-Thom@users.noreply.github.com>
2023-08-04 20:14:35 -0400

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,7 +21,14 @@ export default defineConfig({
 	workers: process.env.CI ? 1 : undefined,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
 	reporter: "html",
-	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+	/* Shared settings for all the projects below. 
+ https://playwright.dev/docs/api/class-testoptions. */
+webServer: {
+    command: 'npm run start',
+    url: 'http://127.0.0.1:3000',
+    reuseExistingServer: !process.env.CI,
+    stdout: 'ignore',
+    stderr: 'pipe',
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
 		// baseURL: 'http://127.0.0.1:3000',
