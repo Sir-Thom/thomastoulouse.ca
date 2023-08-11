e3972685d37bbfd6c224715f62df21b9c6c20f53
Sir-Thom <Sir-Thom1702@pm.me>
2023-08-11 14:55:07 -0400

    update

--- playwright.config.ts
+++ playwright.config.ts
@@ -77,8 +77,6 @@ export default defineConfig({
 	webServer: {
 		command: "npm run start",
 		url: "http://127.0.0.1:3000",
-		stdout: "ignore",
-		stderr: "pipe",
 		reuseExistingServer: !process.env.CI
 	}
 });
