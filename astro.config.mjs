f58813e81dad5b978136f7614f4b6384bb134e8c
Sir-Thom <Sir-Thom1702@pm.me>
2024-02-12 21:07:37 -0500

    change netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,21 +3,24 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import { loadEnv } from "vite";
 import dotenv from "dotenv";
-import netlify from "@astrojs/netlify/functions";
+import netlify from "@astrojs/netlify";
 dotenv.config();
-const { DOWNLOAD_URL } = loadEnv(import.meta.env.DOWNLOAD_URL, process.cwd(), "");
+const {
+  DOWNLOAD_URL
+} = loadEnv(import.meta.env.DOWNLOAD_URL, process.cwd(), "");
+
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://www.thomastoulouse.ca",
-	integrations: [tailwind(), react()],
-	output: "hybrid",
-	adapter: netlify(),
-	server: {
-		host: "127.0.0.1",
-		port: 8080
-	},
-	env: {
-		DOWNLOAD_URL: DOWNLOAD_URL,
-	}
+  site: "https://www.thomastoulouse.ca",
+  integrations: [tailwind(), react()],
+  output: "hybrid",
+  adapter: netlify(),
+  server: {
+    host: "127.0.0.1",
+    port: 8080
+  },
+  env: {
+    DOWNLOAD_URL: DOWNLOAD_URL
+  }
 });
